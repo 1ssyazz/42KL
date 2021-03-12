9fc5631e584c901a95ffb19baa5203ad6b669a55
Exam 42 <exam@u90z04s06.42kl.edu.my>
2021-03-12 17:05:25 +0800

    15

--- alpha_mirror/alpha_mirror.c
+++ alpha_mirror/alpha_mirror.c
@@ -3,28 +3,20 @@
 
 int		main(int argc, char *argv[])
 {
-	char		str;
 	int			i;
 
 	i = 0;
-	if (argc != 2)
+	if (argc == 2)
 	{
-		write(1, "\n", 1);
-		return (0);
-	}
-	while (argv[1][i])
-	{
-		if (97 <= argv[1][i] && 122 >= argv[1][i])
+		while (argv[1][i])
 		{
-			str = 97 + (122 - argv[1][i]);
-			write(1, &str, 1);
+			if (argv[1][i] >= 97 && argv[1][i] <= 122)
+				argv[1][i] = 122 - (argv[1][i] - 97);
+			else if (argv[1][i] >= 65 && argv[1][i] <= 90)
+				argv[1][i] = 90 - (argv[1][i] - 65);
+			write(1, &argv[1][i], 1);
+			i++;
 		}
-		else if (65 <= argv[1][i] && 90 >= argv[1][i])
-		{
-			str = 65 + (90 - argv[1][i]);
-			write(1, &str, 1);
-		}
-		i++;
 	}
 	write(1, "\n", 1);
 	return (0);
