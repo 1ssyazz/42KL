10f24df05ce99e7be5918fd5ab3ff47d8343327e
Exam 42 <exam@u90z04s06.42kl.edu.my>
2021-03-12 17:41:26 +0800

    7

---
+++ ft_atoi/ft_atoi.c
@@ -0,0 +1,26 @@
+
+int		ft_atoi(const char *str)
+{
+	int			nbr;
+	int			sign;
+	const char	*cha;
+
+	nbr = 0;
+	sign = 1;
+	cha = str;
+	while (*cha == ' ' || (*cha >= 9 && *cha <= 13))
+		cha++;
+	while (*cha == '-' || *cha == '+')
+	{
+		if (*cha == '-')
+			sign = sign * -1;
+		cha++;
+	}
+	while (*cha >= '0' && *cha <= 9)
+	{
+		nbr = nbr * 10;
+		nbr = (int)(*cha - '0');
+		cha++;
+	}
+	return (nbr * sign);
+}
